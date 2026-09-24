484c8429862cfad2514da6038af6b46f971cd3f0
Hasan Hüseyin Kahraman <hasan.huseyin.341@outlook.com>
2026-09-24 23:44:22 +0300

    Add font

--- hasanhuseyin.tr/src/App.jsx
+++ hasanhuseyin.tr/src/App.jsx
@@ -1,11 +1,34 @@
 import './App.css'
-
+import { TypeAnimation } from 'react-type-animation'
 function App() {
 
 
   return (
     <>
-      <div><p>Hello, World!</p></div>
+      <div className="bg-gray-500 min-h-screen text-white flex flex-col justify-center items-center gap-4">
+        
+        <TypeAnimation
+          sequence={[
+            '',
+            3000,
+            'Front-End Development Student',
+            3000,
+            '',
+            'Front-End Development Student',
+            3000,
+            '',
+            3000,
+          ]}
+          wrapper="div"
+          cursor={true}
+          repeat={Infinity}
+          className="font-Montserrat font-light text-5xl mt-5"
+        />
+
+        <p className="font-Montserrat font-semibold text-5xl pt-5">Hasan Hüseyin</p>
+
+
+      </div>
     </>
   )
 }
